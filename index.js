5d68f6d47f2eeb5ee03d0178587bf8312bb5c862
Rahul Mansharamani <rahul.mansharamani.btech2019@sitpune.edu.in>
2021-09-05 02:23:28 +0530

    files changes

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Fuck you!");
 });
 
+app.get("/dashboard", (req, res) => {
+  res.send("Hola dashboard!");
+});
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
